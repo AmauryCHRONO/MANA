795a32ed81c7e862b6bc090155d53ba99ab879e7
Amaury CHRONOWSKI <126451182+AmauryCHRONO@users.noreply.github.com>
2023-03-10 14:30:24 +0100

    Rename Test.m to Test_1.m

--- Test_1.m
+++ Test_1.m
@@ -1,9 +1,9 @@
 clear variables;  % efface les variables 
-close all;        % ferme toutes les fenêtres 
+close all;        % ferme toutes les fenÃªtres 
 clc;              % efface le contenu de la zone de commande  
  
 u=[2,-1,3,7,1];   % vecteur ligne 
-v=[1,4,-3,8,2];   % vecteur ligne (de même dimension que u) 
+v=[1,4,-3,8,2];   % vecteur ligne (de mÃªme dimension que u) 
 [ps,pm1,pm2]= MyFunction(u,v); % appel de la fonction MyFunction 
  
 disp('Produit scalaire :');disp(ps); 
